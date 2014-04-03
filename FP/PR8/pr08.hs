e42ab4af8f3775572582a2ac953a65500ecec528
Timur <doxxerACM@gmail.com>
2014-04-03 15:30:13 +0400

    FP pr8

--- FP/PR8/pr08.hs
+++ FP/PR8/pr08.hs
@@ -1,22 +1,26 @@
 module PR08 where
 --import Control.Applicative
 
-class Functor f => Applicative f where
-    pure :: a -> f a
-    (<*>) :: f (a -> b) -> f a -> f b
-infixl 4 <*>
+-- class Functor f => Applicative f where
+--     pure :: a -> f a
+--     (<*>) :: f (a -> b) -> f a -> f b
+-- infixl 4 <*>
+-- 
+-- (<$>) :: Functor f => (a -> b) -> f a -> f b
+-- (<$>) = fmap
+-- 
+-- instance Applicative ((->)e) where
+--     pure = const
+--     (<*>) f g x = f x (g x)
+--     
+-- instance Applicative (Either a) where
+--     pure = Right
+--     Right g <*> Right x = Right (g x)
+--     Left g <*> Right x = Left g
+--     Right g <*> Left x = Left x
+--     Left g <*> Left x = Left g
 
-instance Applicative ((->)e) where
-    pure = const
-    (<*>) f g x = f x (g x)
-
-instance Applicative (Either a) where
-    pure = Right
-    Right g <*> Right x = Right (g x)
-    Left g <*> Right x = Left g
-    Right g <*> Left x = Left x
-    Left g <*> Left x = Left g
-            
 main :: IO()
 main = do
-    print $ "OK"
+    --putStrLn $ "OK"
+    print $ Right 2
