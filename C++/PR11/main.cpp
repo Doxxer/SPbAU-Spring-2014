12a2efa1476db15543fce52970c46c7d93f54125
Timur <doxxerACM@gmail.com>
2014-04-26 20:54:00 +0400

    C++ ThreadPool

--- C++/PR11/main.cpp
+++ C++/PR11/main.cpp
@@ -4,46 +4,52 @@
 #include <future>
 #include <queue>
 #include <thread>
-
-#include "queue.hpp"
+#include <mutex>
+#include <condition_variable>
 
 class ThreadPool {
 public:
     ThreadPool(size_t n) : done(false)
     {
         for (size_t i = 0; i < n; ++i) {
-            threads.push_back(std::thread(&ThreadPool::work, this, i));
+            threads.push_back(std::thread(&ThreadPool::work, this));
         }
     }
 
     ~ThreadPool()
     {
         done = true;
-        task_queue.stop();
+        cond_variable.notify_all();
         std::for_each(threads.begin(), threads.end(), std::mem_fun_ref(&std::thread::join));
     }
 
     template <typename Fret, typename... Fargs>
     std::future<Fret> submit(std::function<Fret(Fargs...)> f, Fargs... args)
     {
+        std::lock_guard<std::mutex> lock(mutex_);
         auto ptr = std::make_shared<std::packaged_task<Fret(Fargs...)>>(f);
         task_queue.push([=]() { ptr->operator()(args...); });
+        cond_variable.notify_one();
         return ptr->get_future();
     }
 
 private:
-    multithread_queue<std::function<void()>> task_queue;
+    std::queue<std::function<void()>> task_queue;
     std::vector<std::thread> threads;
+    std::condition_variable cond_variable;
+    std::mutex mutex_;
     bool done;
 
-    void work(int id)
+    void work()
     {
         while (!done) {
-            auto task = task_queue.pop();
-            if (task) {
-                std::cout << "thread #" << id << " ";
-                task->operator()();
-            }
+            std::unique_lock<std::mutex> lock(mutex_);
+            cond_variable.wait(lock, [this] { return done || !task_queue.empty(); });
+            if (done)
+                break;
+            std::function<void()> task = task_queue.front();
+            task_queue.pop();
+            task();
         }
     }
 };
