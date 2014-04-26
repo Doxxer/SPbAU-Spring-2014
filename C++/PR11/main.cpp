267473140db4c453a11e93cba84bb1f439d660ad
Timur <doxxerACM@gmail.com>
2014-04-26 16:21:48 +0400

    C++ ThreadPool try2

--- C++/PR11/main.cpp
+++ C++/PR11/main.cpp
@@ -5,61 +5,60 @@
 #include <queue>
 #include <thread>
 
+#include "queue.hpp"
+
 class ThreadPool {
 public:
     ThreadPool(size_t n) : done(false)
     {
         for (size_t i = 0; i < n; ++i) {
-            threads.push_back(std::thread(&ThreadPool::work, this));
+            threads.push_back(std::thread(&ThreadPool::work, this, i));
         }
     }
 
     ~ThreadPool()
     {
         done = true;
+        task_queue.stop();
         std::for_each(threads.begin(), threads.end(), std::mem_fun_ref(&std::thread::join));
     }
 
     template <typename Fret, typename... Fargs>
     std::future<Fret> submit(std::function<Fret(Fargs...)> f, Fargs... args)
     {
-        auto ptr = std::make_shared<std::packaged_task<Fret(Fargs...)> >(f);
-        std::lock_guard<std::mutex> lock(mutex);
+        auto ptr = std::make_shared<std::packaged_task<Fret(Fargs...)>>(f);
         task_queue.push([=]() { ptr->operator()(args...); });
         return ptr->get_future();
     }
 
 private:
-    std::queue<std::function<void()> > task_queue;
+    multithread_queue<std::function<void()>> task_queue;
     std::vector<std::thread> threads;
     bool done;
-    std::mutex mutex;
 
-    void work()
+    void work(int id)
     {
         while (!done) {
-            std::lock_guard<std::mutex> lock(mutex);
-            if (!task_queue.empty()) {
-                auto task = task_queue.front();
-                task_queue.pop();
-                task();
+            auto task = task_queue.pop();
+            if (task) {
+                std::cout << id << " ";
+                task->operator()();
             }
         }
     }
 };
 
-int foo(int a)
+void foo(int a)
 {
-    ++a;
+    std::this_thread::sleep_for(std::chrono::milliseconds(a));
     std::cout << a << std::endl;
-    return a;
 }
 
 int main()
 {
     ThreadPool tp(10);
-    for (int i = 0; i < 20; ++i) {
-        tp.submit(std::function<int(int)>(foo), i * 20);
+    for (int i = 0; i < 25; ++i) {
+        tp.submit(std::function<void(int)>(foo), i * 20).get();
     }
 
     return 0;
