b4ba791a3bb9d864218297f64b7c1266d528dfc9
Timur <doxxerACM@gmail.com>
2014-05-29 02:40:36 +0400

    Java test project

---
+++ Java/test/src/ru/spbau/turaevT/Main.java
@@ -0,0 +1,21 @@
+package ru.spbau.turaevT;
+
+class MyObject {
+
+}
+
+class Fruit extends MyObject {
+
+}
+
+class Apple extends Fruit {
+
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+}
